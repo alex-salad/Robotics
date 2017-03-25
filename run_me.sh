c943f409f9fd3a6641ccc02b1e48b9569f4222a5
alex <alex.salazaralmaraz-1@ou.edu>
2017-03-25 14:16:42 -0500

    removed cd

--- run_me.sh
+++ run_me.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd $HOME/robotics
 catkin_make
 source devel/setup.bash
 roslaunch project1 project1.launch
--- run_me2.sh
+++ run_me2.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd $HOME/robotics
 source devel/setup.bash
 rosrun project1 explore
