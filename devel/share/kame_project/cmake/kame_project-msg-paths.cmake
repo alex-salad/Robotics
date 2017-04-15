5b36c4d7a018ae959e9e6a1212f3efef4b07707b
alex <alex.salazaralmaraz-1@ou.edu>
2017-04-15 15:33:54 -0500

    clean up

---
+++ devel/share/kame_project/cmake/kame_project-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(kame_project_MSG_INCLUDE_DIRS "")
+set(kame_project_MSG_DEPENDENCIES geometry_msgs;kobuki_msgs;std_msgs;sensor_msgs)
