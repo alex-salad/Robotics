c297b011d660949e85a56f445c92ee303595f45a
alex <alex.salazaralmaraz-1@ou.edu>
2017-04-18 16:32:20 -0500

    implemented untested detect

--- src/kame_project/src/explore.cpp
+++ src/kame_project/src/explore.cpp
@@ -3,19 +3,21 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "kobuki_msgs/BumperEvent.h"
+#include "sensor_msgs/LaserScan.h"
 #include <math.h>
 #include <boost/thread/thread.hpp>
-#include "sensor_msgs/LaserScan.h"
 
 // constants
 // --------------------------------------------------------
 #define TURN_ANGLE M_PI / 12
 #define ESCAPE_ANGLE M_PI * 5.0 / 6.0
 #define SPEED 0.2
+
 #define FRONT_OBSTACLE 0
 #define RIGHT_OBSTACLE 1
 #define LEFT_OBSTACLE 2
 #define NO_OBSTACLE -1
+
 #define DELTA 0.15
 
 
@@ -34,14 +36,14 @@ private:
     bool canDrive;
     double distance_counter;
     int cooldown;
+
     // private functions
     void halt(const kobuki_msgs::BumperEvent::ConstPtr &msg);
-    void rotate(double angle, double angular_vel, bool &condition);
     void keyboard(const geometry_msgs::Twist::ConstPtr &msg);
+    int detect(const sensor_msgs::LaserScanConstPtr &msg);
+    void avoid()
     void turn();
     void drive();
-
-    void detect(const sensor_msgs::LaserScanConstPtr &msg);
 
 public:
     // public functions
@@ -58,7 +60,6 @@ public:
 */
 Explorer::Explorer(ros::NodeHandle *n) {
     this->n = n;
-    // publisher for auto movement
     p = n->advertise<geometry_msgs::Twist>("/mobile_base/commands/velocity", 10);
     canTurn = true;
     canDrive = true;
@@ -77,13 +78,35 @@ Explorer::~Explorer() {}
 // HALT FEATURE
 // ========================================================
 /**
-* Handles bumber events
+* Shutsdown when a bumper has been pressed
 */
 void Explorer::halt(const kobuki_msgs::BumperEvent::ConstPtr &msg) {
     // shut down the system! Turtle bot has died!
     if (msg->state == kobuki_msgs::BumperEvent::PRESSED) {
-	ROS_INFO("Man down!");
+	ROS_INFO("Man down, we need a medic!");
         ros::shutdown();
+    }
+}
+
+// ========================================================
+// KEYBOARD FEATURE
+// ========================================================
+/**
+* Disables other features to enable somewhat uninterupted control
+* Enables feautes when no keyboard input that moves
+*/
+void Explorer::keyboard(const geometry_msgs::Twist::ConstPtr &msg) {
+    // disable features and publish command
+    if (msg->linear.x != 0 || msg->angular.z != 0) {
+        canTurn = false;
+        canDrive = false;
+        p.publish(*msg);
+    }
+
+    // enable features
+    else {
+        canTurn = true;
+        canDrive = true;
     }
 }
 
@@ -91,62 +114,59 @@ void Explorer::halt(const kobuki_msgs::BumperEvent::ConstPtr &msg) {
 // DETECT FEATURE
 // ========================================================
 /**
-* detects if there are any obstacle and where they are from
+* Determines where the obstacle comes from
 */
-void Explorer::detect(const sensor_msgs::LaserScanConstPtr &msg) {
-    ROS_INFO("FUCK THIS %f", msg->range_min);
-}
+int Explorer::detect(const sensor_msgs::LaserScanConstPtr &msg) {
+    double[] ranges = msg->ranges;
+    double left_min = 1.0;
+    double right_min = 1.0;
 
-// ========================================================
-// ROTATE FEATURE
-// ========================================================
-/**
- * publishes commands for rotating the turtle bot
- */
-void Explorer::rotate(double angle, double angular_vel, bool &condition) {
-    // message for rotating
-    geometry_msgs::Twist rotate_cmd;
-    // not moving
-    rotate_cmd.linear.x = 0;
-    rotate_cmd.linear.y = 0;
-    rotate_cmd.linear.z = 0;
-    // only rotating
-    rotate_cmd.angular.z = angular_vel;
+    for (int i = 0; i < ranges.size() < i++) {
+        if (ranges[i] <= msg->range_min || ranges[i] >= msg->rang_max) {
+            continue;
+        }
 
-    double current_angle = 0;
-    double start_time = ros::Time::now().toSec();
-    
-    // run at 10Hz
-    ros::Rate loop_rate(10); 
-    // get a random direction for turning
-    while (ros::ok() &&  condition && current_angle < angle) {
-        p.publish(rotate_cmd);
-        current_angle =  fabs(angular_vel) * ((ros::Time::now().toSec()) - start_time);
-        loop_rate.sleep();
+        double angle = msg->min_anle + i * msg->angle_increment;
+
+        // if distance is on the right
+        if (angle < 0) {
+            right_min = (right_min < ranges[i]) ? right_min : ranges[i];
+        } else {
+            left_min = (left_min < ranges[i]) ? left_min : ranges[i];
+        }
     }
 
-
-}
-
-// ========================================================
-// KEYBOARD FEATURE
-// ========================================================
-/**
-* Deals with the keyboard commands from teleop keyboard
-*/
-void Explorer::keyboard(const geometry_msgs::Twist::ConstPtr &msg) {
-    // disable features and publish command
-    if (msg->linear.x != 0 || msg->angular.z != 0) {
-        canTurn = false;
-        canDrive = false;
-	
-	p.publish(*msg);
+    if (left_min > 1 + DELTA && right_min > 1 + DELTA) {
+        ROS_INFO("NO OBSTACLE :)");
+        return NO_OBSTACLE;
     }
-    // enable features
+
+    if (fabs(right_min - left_min) < DELTA) {
+        ROS_INFO("OBSTACLE DETECTED AT FRONT!");
+        return FRONT_OBSTACLE;
+    }
+
+    else if (right_min < left_min) {
+        ROS_INFO("OBSTACLE DETECTED AT RIGHT!");
+        return RIGHT_OBSTACLE;
+    }
+
     else {
-        canTurn = true;
-        canDrive = true;
+        ROS_INFO("OBSTACLE DETECTED AT LEFT!");
+        return LEFT_OBSTACLE;
     }
+
+    return NO_OBSTACLE;
+}
+
+// ========================================================
+// AVOID FEATURE
+// ========================================================
+/**
+* Determines where the obstacle comes from
+*/
+int Explorer::detect(const sensor_msgs::LaserScanConstPtr &msg) {
+    int status = detect(msg);
 }
 
 // ========================================================
@@ -164,20 +184,26 @@ void Explorer::turn() {
     turn_cmd.linear.y = 0;
     
     // run at 10z
-    ros::Rate loop_rate(10);    
+    ros::Rate loop_rate(10);
+
+    // loop until ros quits
     while (ros::ok()) {        
         // make sure drive is enabled while not currently turning
         if (canTurn) {
             canDrive = true;
         }
-        double start_time = ros::Time::now().toSec();
-        // turn after 1m of moving and turn off driving while publishing turn command
+
+        // check if it is time to turn
         if (distance_counter >= 1 && canTurn) {
+            // shut off driving
             canDrive = false;
             double current_angle = 0;
             double start_time = ros::Time::now().toSec();
+
             // get a random direction for turning
             turn_cmd.angular.z = (rand() % 2) ? angular_vel : -1 * angular_vel;
+
+            // publish turn commands
             while (ros::ok() && canTurn && current_angle < TURN_ANGLE) {
                 p.publish(turn_cmd);
                 current_angle =  angular_vel * ((ros::Time::now().toSec()) - start_time);
@@ -194,7 +220,7 @@ void Explorer::turn() {
 // DRIVE FEATURE
 // ========================================================
 /**
-* Publishes commands for traveling in a straigh line for one meter.
+* Publishes commands for traveling in a straight line for one meter.
 */
 void Explorer::drive() {
     // the move to be published: STRAIGHT ONLY
@@ -206,16 +232,18 @@ void Explorer::drive() {
     // run at 10Hz
     ros::Rate loop_rate(10);
     
-    // enters loop until ros quits
+    // loops until ros quits
     while (ros::ok()) {
         double start_time = ros::Time::now().toSec();
-        double current_distance = 0;        
+        double current_distance = 0;
+
         // publishes command for moving 1 meter at a time
         while (ros::ok() && canDrive && current_distance < 1) {
             p.publish(move_cmd);
             current_distance = SPEED * (ros::Time::now().toSec() - start_time);
             loop_rate.sleep();
         }
+
         // update distance traveled
         distance_counter += current_distance;
         loop_rate.sleep();
@@ -232,13 +260,16 @@ void Explorer::explore() {
     // create subscibers
     ros::Subscriber halt_sub = n->subscribe("mobile_base/events/bumper", 20, &Explorer::halt, this);
     ros::Subscriber keyboard_sub = n->subscribe("keyboard_controls", 100, &Explorer::keyboard, this);
-    ros::Subscriber escape_sub = n->subscribe("scan", 2, &Explorer::detect, this);
+    ros::Subscriber avoid_sub = n->subscribe("scan", 2, &Explorer::avoid, this);
+
     // start threads
     boost::thread turn_thread(&Explorer::turn, this);
     boost::thread drive_thread(&Explorer::drive, this);
+
     // detach threads
     turn_thread.detach();
     drive_thread.detach();
+
     // spin away
     ros::spin();
 }
