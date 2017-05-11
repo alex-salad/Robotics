ba63711e296eee2de756eed71a58525047bbe63f
Alex <alex.salazaralmaraz-1@ou.edu>
2017-05-11 14:17:43 -0500

    dependencies and stub code

---
+++ src/kame_project/src/tour.cpp
@@ -0,0 +1,13 @@
+#include "ros/ros.h"
+#include "move_base_msgs/MoveBaseAction.h"
+#include "actionlib/client/simple_action_client.h"
+
+int main (int argc, char **argv) {
+    ros::init(argc, argv, "tour");
+    ros::NodeHandle n;
+    
+    //TODO fill stuff out
+    
+    return 0;
+}
+
